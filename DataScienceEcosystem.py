7b6cff9bd3e48bcf7455fea505ff46ee1bf89e06
Alvaro Wang <155372511+Alvs87@users.noreply.github.com>
2024-01-02 12:27:50 -0300

    Add files via upload
    Here is a practice notebook created while studying the IBM Data Science Course

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - List of popular languages for Data Science
+# - List of common libraries for Data Science
+# - Creating tables
+# - Evaluating basic arithmetic expressions
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Pandas
+# 2. Matplotlib
+# 3. NumPy
+# 4. Pytorch
+# 5. Ggplot
+
+# Data Science Tools|
+# |----|
+# Jupyter Notebooks
+# R Studio
+# Kibana
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# This is a simple arithmetic expression to multiply then add integers.
+
+# In[10]:
+
+
+exp = int((3*4) + 5)
+print(exp)
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+
+# In[9]:
+
+
+min = 200
+hours = float(min/60)
+print(hours)
+
+
+# **Author:** 
+
+# Alvaro Wang
+
+# 
